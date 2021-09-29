5c8e617346744142dc066fd4d63244b89ee459a0
Michael Mayer <mjmayer@ucdavis.edu>
2021-09-29 11:22:55 -0700

    Add user friend iam names

--- global/iam/main.tf
+++ global/iam/main.tf
@@ -11,7 +11,12 @@ terraform {
   }
 }
 
+variable "user_names" {
+  description = "Create IAM users with these names"
+  type        = list(string)
+  default     = ["neo", "trinity", "morpheus"]
+}
 resource "aws_iam_user" "example" {
-  count = 3
-  name  = "neo.${count.index}"
+  count = length(var.user_names)
+  name  = var.user_names[count.index]
 }
