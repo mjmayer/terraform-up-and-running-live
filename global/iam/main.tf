c71a3b402edfcb7887584e2004fea634d2665ec1
Michael Mayer <mjmayer@ucdavis.edu>
2021-09-29 11:46:33 -0700

    Remove trinity

--- global/iam/main.tf
+++ global/iam/main.tf
@@ -14,7 +14,7 @@ terraform {
 variable "user_names" {
   description = "Create IAM users with these names"
   type        = list(string)
-  default     = ["neo", "trinity", "morpheus"]
+  default     = ["neo", "morpheus"]
 }
 resource "aws_iam_user" "example" {
   for_each = toset(var.user_names)
